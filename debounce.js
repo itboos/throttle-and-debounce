99d884759499afe22f44e84d7e8e36a4f565ca0f
itboos <zhongdonling@gmail.com>
2021-02-03 14:21:03 +0800

    feat: update

--- debounce.js
+++ debounce.js
@@ -1,54 +1,53 @@
 /**
  * 防抖函数，可以决定是开头调用还是末尾调用， 在指定的时间内，只会调用一次，如果在时间间隔内又
  * 触发了函数，则重新计算时间
- * @param {Number} delay 
- * @param {Function} callback 
+ * @param {Number} delay
+ * @param {Function} callback
  * @param {Boolean} [atBegin] 是否头部调用, 默认为 false
  * @return {Function}  A new, debounce, function.
  */
 export default function debounce(delay, callback, atBegin = false) {
-  if (typeof callback != 'function') {
-    throw new Error('callback must be function.')
+  if (typeof callback != "function") {
+    throw new Error("callback must be function.");
   }
 
-  let lastExec = 0
-  let timeId = -1
-  let canceled = false
-
+  let lastExec = 0;
+  let timeId = -1;
+  let canceled = false;
 
   function clearExistingTimeout() {
-    if(timeId >= 0) {
-      clearTimeout(timeId)
+    if (timeId >= 0) {
+      clearTimeout(timeId);
     }
   }
 
   function wrapper(...args) {
-    let self = this
-    let intervalTime = Date.now() - lastExec
+    let self = this;
+    let intervalTime = Date.now() - lastExec;
 
     if (canceled) {
-      return
+      return;
     }
-    
-    clearExistingTimeout()
+
+    clearExistingTimeout();
 
     if (atBegin && intervalTime > delay) {
-      exec()
-      return
+      exec();
+      return;
     }
     // 使用 setTimeout 延迟执行函数
     timeId = setTimeout(exec, delay);
 
     function exec() {
-      lastExec =  Date.now()
-      callback.apply(self, args)
+      lastExec = Date.now();
+      callback.apply(self, args);
     }
   }
 
   wrapper.cancel = function () {
-    clearExistingTimeout()
-    canceled = true
-  }
+    clearExistingTimeout();
+    canceled = true;
+  };
 
-  return wrapper
+  return wrapper;
 }
--- throttle.js
+++ throttle.js
@@ -1,52 +1,52 @@
 /**
- *  节流函数，频繁触发的函数，在每个一段时间内才调用一次
+ *  节流函数，频繁触发的函数，在每隔一段时间内才调用一次
  * @param {Number} delay 节流函数的每次触发时间 毫秒
  * @param {Function} callback 要节流的函数
  * @param {Boolean} [isTrailing]  是否尾部调用最后一次触发函数
  * @return {Function} A new, throttled, function
  */
 export default function throttle(delay, callback, isTrailing = false) {
-  if (typeof callback != 'function') {
-    throw new Error('callback must be function.')
+  if (typeof callback != "function") {
+    throw new Error("callback must be function.");
   }
 
   let lastExec = 0;
   let timeId = -1;
-  let canceled = false
+  let canceled = false;
 
   function clearExistingTimeout() {
     if (timeId >= 0) {
-      clearTimeout(timeId)
+      clearTimeout(timeId);
     }
   }
 
   function wrapper(...args) {
-    let self = this
-    let intervalTime = Date.now() - lastExec
+    let self = this;
+    let intervalTime = Date.now() - lastExec;
 
     if (canceled) {
-      return
+      return;
     }
-    
-    clearExistingTimeout()
+
+    clearExistingTimeout();
 
     if (intervalTime > delay) {
-      exec()
+      exec();
     } else if (isTrailing) {
       // 尾部调用 为 true， 执行最后一次调用
-      timeId = setTimeout(exec, delay - intervalTime)
+      timeId = setTimeout(exec, delay - intervalTime);
     }
 
     function exec() {
-      lastExec = Date.now()
-      callback.apply(self, args)
-   }
+      lastExec = Date.now();
+      callback.apply(self, args);
+    }
   }
 
-  wrapper.cancel = function() {
-    clearExistingTimeout()
-    canceled = true
-  }
+  wrapper.cancel = function () {
+    clearExistingTimeout();
+    canceled = true;
+  };
 
-  return wrapper
+  return wrapper;
 }
